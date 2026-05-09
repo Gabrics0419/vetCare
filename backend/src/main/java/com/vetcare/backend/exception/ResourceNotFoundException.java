7bd1969df4a03e2e7d0832451318e304652e539e
Gabrics0419 <166676802+Gabrics0419@users.noreply.github.com>
2026-05-08 20:43:33 -0600

    Delete backend directory

--- backend/src/main/java/com/vetcare/backend/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.vetcare.backend.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
